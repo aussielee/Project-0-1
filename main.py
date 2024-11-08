7bc3e0e7fe4ba41892254c5accf53094bc6775a4
aussielee <50060992+aussielee@users.noreply.github.com>
2024-11-08 14:56:08 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 # CarFinder Program
 
 def main():
-    # Display program header
-    print("********************************")
-    print("AutoCountry Vehicle Finder v0.1")
-    print("********************************")
     
     # Initialize list of allowed vehicles
     AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
@@ -12,10 +8,16 @@ def main():
     # Main program loop
     while True:
         # Display menu options
+        print("********************************")
+        print("AutoCountry Vehicle Finder v0.2")
+        print("********************************")
         print("Please Enter the following number below from the following menu:")
+        print(" ")
         print("1. PRINT all Authorized Vehicles")
-        print("2. Exit")
-        choice = input("Enter your choice (1-2): ")
+        print("2. SEARCH for Authorized Vehicle")
+        print("3. Exit")
+        print("********************************")
+        choice = input("Enter your choice (1-3): ")
         
         # Handle user selection
         if choice == '1':
@@ -24,6 +26,13 @@ def main():
             for vehicle in AllowedVehiclesList:
                 print(vehicle)
         elif choice == '2':
+            # Search for a specific vehicle
+            search_query = input("Please Enter the full Vehicle name: ")
+            if search_query in AllowedVehiclesList:
+                print(f"\033[1m{search_query} is an authorized vehicle.\033[0m")
+            else:
+                print(f"\033[1m{search_query} is not an authorized vehicle, if you received this in error please check the spelling and try again.\033[0m")
+        elif choice == '3':
             # Exit program
             print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
             input("Press any key to exit...")
